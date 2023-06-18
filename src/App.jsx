4952ab2caa1ed6ea9f80817decea2afee1e14eb8
Upendra2003 <upendrakarimi2003@gmail.com>
2023-06-18 20:36:39 +0530

    made some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './index.css'
 import Home from './Pages/Home/Home'
 import About from './Pages/About/About'
 import Education from './Pages/Education/Education'
@@ -6,17 +7,33 @@ import Experience from './Pages/Experience/Experience'
 import Skills from './Pages/Skills/Skills'
 import Contact from './Pages/Contact/Contact'
 import ScrollToTop from '../src/Components/ScrollToTop/ScrollToTop'
+import GridLoader from "react-spinners/GridLoader";
 
 export default function App() {
+  const [loading,setLoading]=useState(false)
+  useEffect(()=>{
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 3000);
+  },[]);
   return (
     <>
-      <Home/>
-      <About/>
-      <Education/>
-      <Experience/>
-      <Skills/>
-      <Contact/>
-      <ScrollToTop/>
+      {
+        loading?
+        <div className="app-container">
+          <GridLoader color="#00B2FF" />
+        </div>:
+        <>
+          <Home/>
+          <About/>
+          <Education/>
+          <Experience/>
+          <Skills/>
+          <Contact/>
+          <ScrollToTop/>
+        </>
+      }
     </>
   )
 }
