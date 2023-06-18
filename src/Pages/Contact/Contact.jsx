bc96be935d20079b6485e3e792183c2a2b67e4a1
Upendra2003 <upendrakarimi2003@gmail.com>
2023-06-18 22:05:52 +0530

    few changes

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import './Contact.css'
 import FooterIcons from '../../Components/FooterIcons/FooterIcons'
 import Footer from '../../Components/Footer/Footer'
-import axios from 'axios'
+import emailjs from 'emailjs-com';
 
 
 export default function Contact() {
@@ -14,16 +14,22 @@ export default function Contact() {
   const handleOnChange=e=>{
     setData({...data,[e.target.name]:[e.target.value]})
   }
-  const handleOnSubmit=e=>{
+  const sendEmail = (e)=>{
     e.preventDefault();
-    axios.post('https://portfolio-a896d-default-rtdb.firebaseio.com/messages.json',data).then(console.log("Successfully sent data"))
+    emailjs.sendForm('service_wr2p8hd', 'template_i8bsy9p', e.target, 'w_DqOPnI8weFYwGsa')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset();
   }
   return (
     <>
       <div className="contact-section" id='contact'>
         <h2 className='heading'>LET’S CONNECT !</h2>
         <p className='msg-para'>Thank you for your interest and for taking the time to connect. I look forward to hearing from you and discussing how we can make your web design and development aspirations a reality! </p>
-        <form onSubmit={handleOnSubmit} className='flex w-full flex-col'>
+        <form onSubmit={sendEmail} className='flex w-full flex-col'>
           <div className='inputs'>
             <div className="name-input">
               <label>Name</label>
